d563bb5bc802168cea12242c795c4476d37f1af8
Feldwor <BoQsc@users.noreply.github.com>
2021-08-07 13:25:08 +0300

    Create xfce.bash

---
+++ xfce.bash
@@ -0,0 +1,4 @@
+Compile Docbook-xml-dtd
+InstallPackage https://gobolinux.org/packages/017/Fuse--2.9.7--x86_64.tar.bz2
+InstallPackage https://gobolinux.org/packages/017/UnionFS-Fuse--2.1--x86_64.tar.bz2
+Compile xfce 4.14
