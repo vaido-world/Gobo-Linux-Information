4de035432fb8b122a4546647b9eaa781bfae69c6
Feldwor <BoQsc@users.noreply.github.com>
2021-08-12 18:56:01 +0300

    Update install.bash

--- XFCE-Package/install.bash
+++ XFCE-Package/install.bash
@@ -1,9 +1,6 @@
 curl -L "https://github.com/vaido-world/gobo/raw/main/XFCE-Package/XFCE--4.14--x86_64.tar.bz2" -O
 InstallPackage "XFCE--4.14--x86_64.tar.bz2"
 
-curl -L "https://github.com/vaido-world/gobo/raw/main/XFCE-Package/XFCE4-Session--4.14.2--x86_64.tar.bz2" -O
-InstallPackage "XFCE4-Session--4.14.2--x86_64.tar.bz2"
-
 
 curl -L "https://github.com/vaido-world/gobo/raw/main/XFCE-Package/XFCE4-Settings--4.14.3--x86_64.tar.bz2" -O
 InstallPackage "XFCE4-Settings--4.14.3--x86_64.tar.bz2"
