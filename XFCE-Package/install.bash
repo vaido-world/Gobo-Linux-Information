4e23e4629637cbbb98aea3ab5ba84de732c4bad5
Feldwor <BoQsc@users.noreply.github.com>
2021-08-12 18:51:50 +0300

    Create install.bash

---
+++ XFCE-Package/install.bash
@@ -0,0 +1,24 @@
+curl -L "https://github.com/vaido-world/gobo/raw/main/XFCE-Package/XFCE--4.14--x86_64.tar.bz2" -O
+InstallPackage "XFCE--4.14--x86_64.tar.bz2"
+
+curl -L "https://github.com/vaido-world/gobo/raw/main/XFCE-Package/XFCE4-Session--4.14.2--x86_64.tar.bz2" -O
+InstallPackage "XFCE4-Session--4.14.2--x86_64.tar.bz2"
+
+
+curl -L "https://github.com/vaido-world/gobo/raw/main/XFCE-Package/XFCE4-Settings--4.14.3--x86_64.tar.bz2" -O
+InstallPackage "XFCE4-Settings--4.14.3--x86_64.tar.bz2"
+
+
+curl -L "https://github.com/vaido-world/gobo/raw/main/XFCE-Package/LibXFCE4UI--4.14.1--x86_64.tar.bz2" -O
+InstallPackage "LibXFCE4UI--4.14.1--x86_64.tar.bz2"
+
+
+curl -L "XFCE4-Session--4.14.2--x86_64.tar.bz2" -O
+InstallPackage "XFCE4-Session--4.14.2--x86_64.tar.bz2"
+
+
+curl -L "XFCE4-Settings--4.14.3--x86_64.tar.bz2" -O
+InstallPackage "XFCE4-Settings--4.14.3--x86_64.tar.bz2"
+
+curl -L "XFCE4-Terminal--0.8.9--x86_64.tar.bz2" -O
+InstallPackage "XFCE4-Terminal--0.8.9--x86_64.tar.bz2"
