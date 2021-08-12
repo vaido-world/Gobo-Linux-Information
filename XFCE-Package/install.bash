8ff21070fe03186ec3f05eb64f08873e9c1a4e5d
Feldwor <BoQsc@users.noreply.github.com>
2021-08-12 19:03:26 +0000

    Update install.bash

--- XFCE-Package/install.bash
+++ XFCE-Package/install.bash
@@ -1,8 +1,10 @@
 InstallPackage https://gobolinux.org/packages/017/Fuse--2.9.7--x86_64.tar.bz2
 InstallPackage https://gobolinux.org/packages/017/UnionFS-Fuse--2.1--x86_64.tar.bz2
 
-# Compile LibXFCE4Util 4.14.0
-Compile LibXFCE4Util 4.14.1
+Compile LibXFCE4Util 4.14.0
+Compile XFConf 4.14.4
+Compile LibXFCE4UI 4.14.1
+
 Compile EXO
 
 
