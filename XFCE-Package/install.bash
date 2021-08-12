73d55bf96c310ad50d46f426c7f029dc3aac492e
Feldwor <BoQsc@users.noreply.github.com>
2021-08-12 18:54:32 +0300

    Update install.bash

--- XFCE-Package/install.bash
+++ XFCE-Package/install.bash
@@ -13,12 +13,29 @@ curl -L "https://github.com/vaido-world/gobo/raw/main/XFCE-Package/LibXFCE4UI--4
 InstallPackage "LibXFCE4UI--4.14.1--x86_64.tar.bz2"
 
 
+curl -L "XFConf--4.14.4--x86_64.tar.bz2" -O
+InstallPackage "XFConf--4.14.4--x86_64.tar.bz2"
+
+curl -L "XFDesktop--4.14.4--x86_64.tar.bz2" -O
+InstallPackage "XFDesktop--4.14.4--x86_64.tar.bz2"
+
 curl -L "XFCE4-Session--4.14.2--x86_64.tar.bz2" -O
 InstallPackage "XFCE4-Session--4.14.2--x86_64.tar.bz2"
-
 
 curl -L "XFCE4-Settings--4.14.3--x86_64.tar.bz2" -O
 InstallPackage "XFCE4-Settings--4.14.3--x86_64.tar.bz2"
 
 curl -L "XFCE4-Terminal--0.8.9--x86_64.tar.bz2" -O
 InstallPackage "XFCE4-Terminal--0.8.9--x86_64.tar.bz2"
+
+
+curl -L "XFCE4-Power-Manager--1.7.0--x86_64.tar.bz2" -O
+InstallPackage "XFCE4-Power-Manager--1.7.0--x86_64.tar.bz2"
+
+curl -L "XFCE4-Panel--4.14.4--x86_64.tar.bz2" -O
+InstallPackage "XFCE4-Panel--4.14.4--x86_64.tar.bz2"
+
+curl -L "XFCE4-AppFinder--4.14.1--x86_64.tar.bz2" -O
+InstallPackage "XFCE4-AppFinder--4.14.1--x86_64.tar.bz2"
+
+
