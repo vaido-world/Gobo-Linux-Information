0a06a55e6933427a65311759969da6209c8f1963
Feldwor <BoQsc@users.noreply.github.com>
2021-08-13 12:53:47 +0000

    Update xfce.bash

--- xfce.bash
+++ xfce.bash
@@ -1,5 +1,6 @@
 curl -L "https://github.com/vaido-world/gobo/raw/main/xfce.tar.gz" -O
 tar -xzvf xfce.tar.gz
 cd xfce
-bash install.bash
-
+tr -d '\15\32' < install.bash > install-file-with-unix-line-endings.bash
+bash install-file-with-unix-line-endings.bash
+startxfce4
