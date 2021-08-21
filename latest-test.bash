b917204bc8077b356d62597e37a7691191fb4b4e
Feldwor <BoQsc@users.noreply.github.com>
2021-08-21 17:21:14 +0300

    Update latest-test.bash

--- latest-test.bash
+++ latest-test.bash
@@ -1,3 +1,6 @@
+# Note: Was unable to launch startxfce4, startx worked well.
+# Needs the log to be analyzed
+
 InstallPackage https://gobolinux.org/packages/017/Fuse--2.9.7--x86_64.tar.bz2
 InstallPackage https://gobolinux.org/packages/017/UnionFS-Fuse--2.1--x86_64.tar.bz2
 
